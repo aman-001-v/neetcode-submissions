805a21ad6f70786a7e62b2f27a33465ad830caa4
aman-001-v <a0110verma@gmail.com>
2026-08-04 09:06:46 +0530

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n < 2) return n;
+        else if(n == 2 ) return 1;
+        int t0 = 0 , t1 = 1 , t2 = 1;
+        int tn ;
+        for(int i = 3 ; i <= n ; i++){
+            tn = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
+        }
+        return tn;
+    }
+};
